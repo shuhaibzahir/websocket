e69a9ac1c4efe3bd347e9dbbfb21316135644a52
shuhaibzahir <yesshuhaib.ss@gmail.com>
2021-10-21 01:56:59 +0530

    websocket io setup

---
+++ server.js
@@ -0,0 +1,61 @@
+const io = require("socket.io")(8900,{
+    cors:{
+        origin:"http://localhost:3000"
+    }
+}) 
+
+// creating a user array 
+
+let users =[]
+
+const addUser =(userId,SocketId)=>{
+
+  !users.some(i=>i.userId==userId)&&users.push({userId,SocketId})
+}
+  
+const deleteUser =  (socketId)=>{
+    users = users.filter(i=>i.SocketId!==socketId)
+}
+
+
+const getUser = (userId)=>{
+  return users.find(user=>user.userId===userId)
+} 
+
+io.on("connection", (socket) => {
+    // when connect 
+    console.log('a user connected')
+
+
+//    take user id and socket id
+    socket.on("addUser",userId=>{
+       
+        addUser(userId,socket.id)
+        io.emit("getUser",users)
+    })
+
+// send and get messages
+
+    socket.on("sendMessage",({senderId,reciverId,text})=>{
+       
+           const user= getUser(reciverId)
+            if(user){
+                io.to(user.SocketId).emit("getMessage",{
+            senderId,
+               text
+             })
+            }
+           
+    })
+
+
+
+
+    // when connect
+    socket.on("disconnect",()=>{
+        
+       deleteUser(socket.id)
+    })
+
+})
+
